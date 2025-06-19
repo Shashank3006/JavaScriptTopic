764ae43c9df8d70103247eb02e433f82d95aff8f
Shashank3006 <shashankguptaab@gmail.com>
2025-06-19 21:01:03 +0530

    string

---
+++ String/revers.js
@@ -0,0 +1,9 @@
+const abc="  hoollooh";
+console.log(abc); //  hoollooh
+let a=abc.split("").reverse().join("");
+console.log(a); // undefinedi
+ a==abc?console.log(true) :console.log(false)
+ abc.charAt(0)=="h"?console.log(abc.charAt(0)):console.log(false);
+ abc.trim()
+  console.log(abc.trim("o")); // 10
+  console.log(abc.split("o")); // 2
